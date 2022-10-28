66712af8b920e79e2f54c3ec3c84a2c81fa048c7
xavimagine <sr.xavier5@gmail.com>
2022-10-28 14:59:19 +0200

    cards

---
+++ js/type.js
@@ -0,0 +1,10 @@
+const cumple = new Date(1991,9,24)
+const yearC = cumple.getFullYear()
+const hoy = new Date().getFullYear();
+const year =  hoy-yearC
+
+new TypeIt(".multipleStrings", {
+    strings: ["Hola,bienvenido", `Me llamao Javier díaz rayo, tengo ${year} años y soy programador y diseñador gráfico`],
+    speed: 50,
+    waitUntilVisible: true,
+  }).go();
